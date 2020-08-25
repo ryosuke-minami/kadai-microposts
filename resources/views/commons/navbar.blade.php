2c00ed10740b22d88cb3df84e873ab48cc70a0cd
ryosuke <e5a2w942cm@outlook.jp>
2020-08-25 15:48:33 +0000

    user regisration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -9,11 +9,11 @@
         
         <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
-            <ul class="navbar-nav">
+            <ul class="nav navbar-nav navbar-light">
                 {{--ユーザー登録ページのリンク--}}
-                <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
+                <li>{!! link_to_route('signup.get','Signup',[],['class'=>'nav-link']) !!}</li>
                 {{--ログインページへのリンク--}}
-                <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
+                <li><a href="#">Login</a></li>
             </ul>
         </div>
     </nav>

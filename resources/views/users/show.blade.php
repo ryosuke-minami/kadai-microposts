f0e1c53cb74f456db380fb6c384fb64a7655ac47
ryosuke <e5a2w942cm@outlook.jp>
2020-08-27 13:50:29 +0000

    user pages

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            <div class="card">
+                <hr class="card-header">
+                <h3 clss="card-title">{{$user->name}}</h3>
+            </div>
+            <div class="card-body">
+                {{--ユーザのメールアドレスをもとにGravatorを取得して表示--}}
+                <img class="rounded img-fluid" src="{{Gravator::get($user->email,['size'=>500])}}" alt="">
+            </div>
+        </aside>
+        <div class="col-sm-8">
+            <ul class="nav nav-tabs nav-justified mb-3">
+                {{--ユーザ詳細タブ--}}
+                <li class="nav-item"><a href="#" class="nav-link">TimeLine</a></li>
+                {{--フォロー一覧タブ--}}
+                <li class="nav-item"><a href="#" class="nav-link">Following</a></li>
+                {{--フォロワー一覧タブ--}}
+                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
+            </ul>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/users/users.blade.php
@@ -0,0 +1,21 @@
+@if(count($users)>0)
+    <ul class="list-unstyled">
+        @foreach($users as $user)
+            <li class="media">
+                {{--ユーザのメールアドレスを元にGravatorを取得して表示--}}
+                <img class="mr-2 rounded" src="{{ Gravator::get($users->email,['size'=>50]) }}" alt="">
+                <div class="media-body">
+                    <div>
+                        {{ $user->name }}
+                    </div>
+                    <div>
+                        {{--ユーザ詳細ページへのリンク--}}
+                        <p>{!! link_to_route('users.show','View profile',['user'=>$user->id]) !!}</p>
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+    {{--ページネーションのリンク--}}
+    {{ $users->links() }}
+@endif

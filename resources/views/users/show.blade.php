7092754548192bcefb8bf5e22987c88a4a3919d1
ryosuke <e5a2w942cm@outlook.jp>
2020-08-29 15:17:25 +0000

    follow,unfollow

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,29 +2,14 @@
 @section('content')
     <div class="row">
         <aside class="col-sm-4">
-            <div class="card">
-                <hr class="card-header">
-                <h3 clss="card-title">{{$user->name}}</h3>
-            </div>
-            <div class="card-body">
-                {{--ユーザのメールアドレスをもとにGravatarを取得して表示--}}
-                <img class="rounded img-fluid" src="{{Gravatar::get($user->email,['size'=>500])}}" alt="">
-            </div>
+            {{--ユーザ情報--}}
+            @include('users.card')
         </aside>
         <div class="col-sm-8">
-            <ul class="nav nav-tabs nav-justified mb-3">
-                {{--ユーザ詳細タブ--}}
-                <li class="nav-item">
-                    <a href="{{route('users.show',['user'=>$user->id])}}" class="nav-link{{Request::routeIs('users.show')?'active':''}}">
-                        TimeLine
-                        <span class="badge badge-secondary">{{$user->microposts_count}}</span>
-                    </a>
-                </li>
-                {{--フォロー一覧タブ--}}
-                <li class="nav-item"><a href="#" class="nav-link">Following</a></li>
-                {{--フォロワー一覧タブ--}}
-                <li class="nav-item"><a href="#" class="nav-link">Followers</a></li>
-            </ul>
+            {{- タブ -}}
+            @include('users.card')
+            {{-- ユーザ一覧 --}}
+            @include('users.users')
             @if(Auth::id()==$user->id)
                 {{-- 投稿フォーム --}}
                 @include('microposts.form')
